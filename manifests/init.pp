bce9aa7197db6430c65ba7964ff5be43448c3520
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-11-06 10:34:21 +0100

    RH7

--- manifests/init.pp
+++ manifests/init.pp
@@ -29,10 +29,20 @@ class tilecache {
       # python-tilecache is not included in RHEL, so we use a working version from
       # the Fedora repository
       case $::operatingsystemmajrelease {
-        '6', '7': {
+        '6': {
           yumrepo {'fedora-core13-for-tilecache':
             descr       => 'Fedora Core 13',
             baseurl     => 'http://dl.fedoraproject.org/pub/archive/fedora/linux/updates/13/$basearch',
+            enabled     => 1,
+            gpgcheck    => 0,
+            includepkgs => 'python-tilecache',
+            before      => Package['python-tilecache'],
+          }
+        }
+        '7': {
+          yumrepo {'fedora-core14-for-tilecache':
+            descr       => 'Fedora Core 14',
+            baseurl     => 'http://dl.fedoraproject.org/pub/archive/fedora/linux/updates/14/$basearch',
             enabled     => 1,
             gpgcheck    => 0,
             includepkgs => 'python-tilecache',
